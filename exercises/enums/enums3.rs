6bde6da2785d367756ce6d178bbf44487f914de9
tangent <tangent0orz@gmail.com>
2024-04-08 21:17:07 +0800

    update

--- exercises/enums/enums3.rs
+++ exercises/enums/enums3.rs
@@ -5,10 +5,13 @@
 // Execute `rustlings hint enums3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 enum Message {
     // TODO: implement the message variant types based on their usage below
+    Move(Point),
+    Echo(String),
+    Quit,
+    ChangeColor(i32,i32,i32),
 }
 
 struct Point {
@@ -43,6 +46,18 @@ impl State {
         // variants
         // Remember: When passing a tuple as a function argument, you'll need
         // extra parentheses: fn function((t, u, p, l, e))
+        match message{
+            Message::ChangeColor(x,y,z)=>{
+                self.color=(x.try_into().unwrap(),y.try_into().unwrap(),z.try_into().unwrap());
+            },
+            Message::Echo(string)=> self.message=string,
+            Message::Move(point)=>{
+                self.position=point;
+            },
+            Message::Quit=>{
+                self.quit=true;
+            }
+        }
     }
 }
 
